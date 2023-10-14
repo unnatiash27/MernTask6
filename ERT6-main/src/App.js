34703d8eff05cb7dc8318a033b175793b92c1636
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-10-14 20:31:03 +0530

    Add files via upload

---
+++ ERT6-main/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ ERT6-main/src/pages/Experience.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+
+function Experience() {
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019-2021"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            City Montessori School
+          </h3>
+          <p> High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2021 - current"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Vellore Institute of Technology Bhopal
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+
+          <p> Computer Science</p>
+        </VerticalTimelineElement> 
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default Experience;
